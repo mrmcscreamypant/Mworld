fd52fd668f41deca0db12bc959b1f16f1ed4e46e
MoeThun <dhtmitmi@gmail.com>
2024-08-26 15:59:02 +0800

    fix: should not show or hide, whenever it's already in the backpack

--- src/gameClasses/Item.js
+++ src/gameClasses/Item.js
@@ -147,6 +147,11 @@ var Item = TaroEntityPhysics.extend({
 				}
 			}
 		}
+		// check if it's in backpack first
+		let owner = self.getOwnerUnit();
+		if (owner && self._stats.slotIndex >= owner._stats.inventorySize) {
+			return;
+		}
 		// if body exists and item is not hidden, show
 		if (body && body.type != 'none') {
 			TaroEntityPhysics.prototype.updateBody.call(self, initTransform);
